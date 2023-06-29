03286adca002a5a9ff5a594241d8e53e2c759304
rostyslaveMuha <rostyslavmushynskyi@gmail.com>
2023-06-29 21:44:31 +0300

    Displayed the data on the page

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,70 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { Button, TextField, styled } from "@mui/material";
+import axios from "axios";
 
-function App() {
-  const [count, setCount] = useState(0)
+const Container = styled("div")(({ theme }) => ({
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  justifyContent: "center",
+  backgroundColor: "lightgrey",
+  height: "200px",
+}));
+
+const App = () => {
+  const [inputValue, setInputValue] = useState("");
+  const [locationData, setLocationData] = useState(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .get(
+        `https://ipgeolocation.abstractapi.com/v1/?api_key=ab632ac2e83343e99ab926fd28196260&ip_address=${inputValue}`
+      )
+      .then((res) => {
+        setLocationData(res.data);
+        console.log(res.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <Container>
+      <h1>IP Geolocation</h1>
+      <form onSubmit={handleSubmit}>
+        <TextField
+          id="standard-basic"
+          label="IP Address"
+          value={inputValue}
+          onChange={handleInputChange}
+        />
+        <Button variant="contained" color="primary" type="submit">
+          Submit
+        </Button>
+      </form>
+      {locationData && (
+        <div>
+          <h2>Location Data</h2>
+          <ul>
+            <li>Country: {locationData.country}</li>
+            <li>Region: {locationData.region}</li>
+            <li>City: {locationData.city}</li>
+            <li>Latitude: {locationData.latitude}</li>
+            <li>Longitude: {locationData.longitude}</li>
+            <li>
+              Flag: <img src={locationData.flag.svg} alt="" />
+            </li>
+          </ul>
+        </div>
+      )}
+    </Container>
+  );
+};
 
-export default App
+export default App;
